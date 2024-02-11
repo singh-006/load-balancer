c2c68e2442cd45e08d7becffe1ddc5312409b739
singh-006 <akash@turfdev.in>
2024-02-11 19:29:41 +0530

    Remove some files

--- elb+application/provider.tf
+++ elb+application/provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAWBUJVDIK2GIWIVHQ"
-  secret_key = "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"
+  access_key = "123"
+  secret_key = "abc"
 }
